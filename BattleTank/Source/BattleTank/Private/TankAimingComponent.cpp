1921040464ed91721ad8e511a435449f1089e4bc
cochaso <cochaso@gmail.com>
2017-06-20 21:57:07 -0300

    no message

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -61,6 +61,8 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
+	if (!Barrel || !Turret) { return; }
+
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
