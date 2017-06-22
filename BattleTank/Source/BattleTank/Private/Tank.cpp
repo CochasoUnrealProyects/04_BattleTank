f41dc6c493b65caed0f153c31aa2e9c839161752
cochaso <cochaso@gmail.com>
2017-06-21 22:38:50 -0300

    Fin del refactor!

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,43 +1,9 @@
 // Copyright Cochanet S.A.
 
 #include "BattleTank.h"
-#include "TankBarrel.h"
-#include "Projectile.h"
 #include "Tank.h"
 
 ATank::ATank()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
-	//auto OurTankName = GetOwner()->GetName();
-	//auto BarrelLocation = Barrel->GetComponentLocation().ToString();
-	//UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation);
-	//UE_LOG(LogTemp, Warning, TEXT("Firing at %s"), LaunchSpeed);
-
-	//Moved to blueprint, dejo esto aca para tenerlo como ejemplo.
-	//TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-}
-
-void ATank::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-void ATank::Fire()
-{
-	if (!ensure(Barrel)) { return; }
-
-	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
-	if (isReloaded)
-	{
-		//Spawn a projectile at socket location on the barrel.
-		auto Projectile = GetWorld()->SpawnActor<AProjectile>(
-			ProjectileBlueprint,
-			Barrel->GetSocketLocation(FName("Projectile")),
-			Barrel->GetSocketRotation(FName("Projectile"))
-			);
-
-		Projectile->LaunchProjectile(LaunchSpeed);
-		LastFireTime = FPlatformTime::Seconds();
-	}
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -5,37 +5,10 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"	//Paste new includes above
 
-class UTankBarrel;
-class AProjectile;
-
 UCLASS()
 class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-private:
-	// Sets default values for this pawn's properties
 	ATank();
-
-	virtual void BeginPlay() override;
-
-	//Local barrel reference for spawning projectile.
-	UTankBarrel* Barrel = nullptr; // TODO Remove.
-
-	double LastFireTime = 0;
-
-public:	
-	UPROPERTY(EditDefaultsOnly, Category = "Setup")
-	TSubclassOf<AProjectile> ProjectileBlueprint;
-	//UClass* ProjectileBlueprint;
-
-	//TODO Remove later
-	UPROPERTY(EditDefaultsOnly, Category = "Firing")
-	float LaunchSpeed = 4000;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Firing")
-	float ReloadTimeInSeconds = 3;
-
-	UFUNCTION(BlueprintCallable, Category = "Firing")
-	void Fire();
 };
